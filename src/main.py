75a130479453dba98319494bccff3633f099afad
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2025-05-25 23:15:25 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print('Welcome to KongChat!')
+print("Welcome to KongChat!")
+while True:
+    msg = input("You: ")
+    if msg == "/quit":
+        print("Goodbye!")
+        break
+    else:
+        print("KongBot: I received your message.")
